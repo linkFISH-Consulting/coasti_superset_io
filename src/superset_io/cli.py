12ac7ee9a2a881f29bf91e723b0278011d9bffa8
pSpitzner <github@makeitso.one>
2026-01-22 18:14:21 +0100

    ruff

--- src/superset_io/cli.py
+++ src/superset_io/cli.py
@@ -1,11 +1,11 @@
 # access_token
-from typing import Annotated, cast
+import logging
+from typing import cast
+
 import requests
 import typer
-import logging
 
 from .api import SuperSetApiClient, SupersetApiSession
-
 
 log = logging.getLogger("superset_io")
 superset_api: SuperSetApiClient | None = None
@@ -34,14 +34,6 @@ def auth(
     if superset_api is not None:
         return
 
-    if username is not None and password is not None and access_token is not None:
-        raise typer.BadParameter(
-            "Provide either username/password or access_token, not both."
-        )
-
-    if not access_token and (username is None or password is None):
-        raise typer.BadParameter("Provide either username/password or access_token.")
-
     if base_url is None:
         base_url = typer.prompt("Base URL", type=str, default="http://localhost:8088")
         base_url = cast(str, base_url)
@@ -69,12 +61,12 @@ def auth(
         )
 
         res.raise_for_status()
-        access_token = res.json().get("access_token")
+        access_token = cast(str, res.json().get("access_token"))
         log.debug("Obtained bearer token")
 
     # csrf
     res = requests.get(
-        f"{base_url}/api/v1/security/csrf_token/",
+        f"{base_url}/api/v1/security/csrf_token",
         headers={
             "Authorization": f"Bearer {access_token}",
             "Content-Type": "application/json",
@@ -85,45 +77,14 @@ def auth(
     csrf_token = res.json().get("result")
 
     session = SupersetApiSession(
-        base_url=base_url, access_token=access_token, csrf_token=csrf_token
+        base_url=base_url,
+        access_token=access_token,
+        csrf_token=csrf_token,
     )
     superset_api = SuperSetApiClient(session)
 
 
 @app.command()
 def main(
-    username: str = "admin",
-    password: str = "admin",
-    base_url: str = "http://localhost:8088",
 ):
     return 0
-    # Auth flow to get tokens for api
-    token = requests.post(
-        f"{base_url}/api/v1/security/login",
-        headers={"Content-Type": "application/json"},
-        json={
-            "username": username,
-            "password": password,
-            "provider": "db",
-            "refresh": True,
-        },
-    )
-
-    # Get crfs token
-    csrf = requests.get(
-        f"{base_url}/api/v1/security/csrf_token",
-        headers={
-            "Authorization": f"Bearer {token.json().get('access_token')}",
-        },
-    )
-
-    # Create api Client
-    api_client = SuperSetApiClient(
-        session=Session(
-            base_url=base_url,
-            access_token=token.json().get("access_token"),
-            csrf_token=csrf.json().get("result"),
-        ),
-    )
-
-    print(api_client.get_dashboards())
